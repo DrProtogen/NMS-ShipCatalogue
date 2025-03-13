70d6ac79fc469910577882fc40f0c171ed3e0616
DrProtogen <sssaklad@gmail.com>
2025-03-13 01:22:06 -0400

    Color

---
+++ js/FIGHTER.js
@@ -0,0 +1,63 @@
+// js/FIGHTER.js
+export const fighterParts = {
+    "wings": ["Wings Droid", "Wings Halo", "Wings Heavy", "Wings Horizon", "Wings Mecha", "Wings Quasar", "Wings Serenity", "Wings Starjumper", "Wings Starscream", "Wings Vector"],
+    "engines": ["Engine Triple", "Engine Box", "Engine Single"],
+    "dorsal": ["Dorsal", "No Dorsal"],
+    "cockpit": ["Cockpit Barrel", "Cockpit Jet", "Cockpit Multi", "Cockpit Snowspeeder", "Cockpit Viper"],
+    "subwings": {
+        "Wings Heavy": ["Bowie", "E-Wing", "Shockwave","Tie","No Subwing"],
+        "Wings Droid": ["Bowie", "E-Wing", "No Subwing"],
+        "Wings Horizon": ["Bowie", "Gull","Tie"],
+        "Wings Mecha": ["Full", "Mid","Low"],
+        "Wings Serenity": ["Bowie", "E-Wing", "Shockwave","Tie","Gull","Vulture"],
+        "Wings Starscream": ["Shockwave","No Subwing"],
+    },
+    "noses": {
+        "Cockpit Multi": ["Nose Alpha", "Nose Long", "Nose Needle", "Nose Rasa", "Nose Stubby"],
+    }
+};
+
+export const partValues = {
+    "wings-droid": "A",
+    "wings-firefly": "B",
+    "wings-halo": "C",
+    "wings-heavy": "D",
+    "wings-horizon": "E",
+    "wings-mecha": "F",
+    "wings-quasar": "G",
+    "wings-serenity": "H",
+    "wings-starjumper": "I",
+    "wings-starscream": "J",
+    "wings-vector": "K",
+
+    "engine-triple": "A",
+    "engine-box": "B",
+    "engine-single": "C",
+
+    "dorsal": "A",
+    "no-dorsal": "B",
+
+    "cockpit-barrel": "A",
+    "cockpit-jet": "B",
+    "cockpit-multi": "C",
+    "cockpit-snowspeeder": "D",
+    "cockpit-viper": "E",
+
+    "nose-alpha": "A",
+    "nose-long": "B",
+    "nose-needle": "C",
+    "nose-rasa": "D",
+    "nose-stubby": "E",
+
+    "bowie": "A",
+    "e-wing": "B",
+    "shockwave": "C",
+    "full": "D",
+    "mid": "E",
+    "low": "F",
+    "vulture": "G",
+    "tie": "H",
+    "aftershock": "I",
+    "gull": "J",
+    "no-subwing": ""
+};
